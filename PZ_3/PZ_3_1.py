a60f21c89d16a450ae3ffdcaec5364a0c7aa5624
zemamirz <zema.mirz@yandex.ru>
2022-11-01 17:48:35 +0300

    Initial commit

--- PZ_3/PZ_3_1.py
+++ PZ_3/PZ_3_1.py
@@ -1,4 +1,26 @@
-a = int(input('Введите число a: '))  #
-b = int(input('Введите число b: '))  #
-c = int(input('Введите число c: '))  #
-print(bool((a > 0) and (b > 0) and (c > 0)))  #
+# Даны три целых числа: A, B, C. Проверить истинность высказывания: "Каждое из чисел A, B, C положительное".
+
+a, b, c = input('Введите число a: '), input('Введите число b: '), input('Введите число c: ')  #
+
+while type(a) != int:  # обработка исключений
+    try:
+        a = int(a)
+    except ValueError:
+        print('Некорректный ввод!')
+        a = input('Введите число a: ')
+
+while type(b) != int:  # обработка исключений
+    try:
+        b = int(b)
+    except ValueError:
+        print('Некорректный ввод!')
+        b = input('Введите число b: ')
+
+while type(c) != int:  # обработка исключений
+    try:
+        c = int(c)
+    except ValueError:
+        print('Некорректный ввод!')
+        c = input('Введите число c: ')
+
+print(bool((a > 0) and (b > 0) and (c > 0)))  # проверка на истинность: положительное или отрицательное
